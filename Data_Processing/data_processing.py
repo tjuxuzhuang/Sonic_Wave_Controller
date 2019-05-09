a30b598f904f561d4281a42d8dc703b6e8c060fe
waybaba <ww15305665971@gmail.com>
2019-05-09 18:48:47 +0800

    finish data preprocessing

--- Data_Processing/data_processing.py
+++ Data_Processing/data_processing.py
@@ -1,8 +1,41 @@
 
-
-
+import config
+import numpy as np
+import random
+from scipy.io import loadmat
 
 def get_data():
-    train_data, train_label, test_data, test_label = [],[],[],[]
+    # fetch raw data
+    fields = []
+    phases = []
+    for i in range(191)[1:]:
+        # data (500,500)
+        fields.append(loadmat(r""+config.data_path + "sonic_data_2/" + "height0.14_"+str(i)+"randfield.mat")['capacity']) # name_template: "height0.14_1randfield.mat"
+        # lable (8,8)
+        phases.append(loadmat(r""+config.data_path + "sonic_data_2/" + "height0.14_"+str(i)+"randphase.mat")['initial_phase']) # name_template: "height0.14_1randphase"
 
-    return train_data,train_label,test_data,test_label
+    # reshape into numpy
+    fields = np.array(fields)
+    phases = np.array(phases)
+
+    # norm field globally
+    fields = fields - np.full(fields.shape,fields.min()) / (np.full(fields.shape,fields.max()) - (np.full(fields.shape,fields.min())))
+
+    # norm phase separately
+    for index_x in range(190):
+        phases[index_x] = (phases[index_x] - np.full(phases[index_x].shape,phases[index_x].min())) / (np.full(phases[index_x].shape,phases[index_x].max()) - np.full(phases[index_x].shape,phases[index_x].min()))
+
+    # split and shuffle
+    state = np.random.get_state()
+    np.random.shuffle(fields)
+    np.random.set_state(state)
+    np.random.shuffle(phases)
+    num_of_date = fields.shape[0]
+    train_num = int((4 * num_of_date) / 5)
+    input_train, input_test, label_train, label_test = fields[0:train_num], fields[train_num:], phases[0:train_num], phases[train_num:]
+    return input_train,label_train,input_test,label_test
+
+
+if __name__ == '__main__':
+    x = get_data()
+    print("finish.")
--- hardware_part/main.py
+++ hardware_part/main.py
@@ -22,10 +22,10 @@ cs      GPIO4  (7) (8)  GPIO14      data4
           GND  (9) (10) GPIO15      data5
 data7   GPIO17 (11) (12) GPIO18
         GPIO27 (13) (14) GND
-        GPIO22 (15) (16) GPIO23
-           3V3 (17) (18) GPIO24
+        GPIO22 (15) (16) GPIO23     choose3
+           3V3 (17) (18) GPIO24     choose4
 data0   GPIO10 (19) (20) GND
-         GPIO9 (21) (22) GPIO25
+         GPIO9 (21) (22) GPIO25     choose5
 data1   GPIO11 (23) (24) GPIO8      # temp_clk
            GND (25) (26) GPIO7      gate0
          GPIO0 (27) (28) GPIO1
@@ -33,8 +33,8 @@ a1       GPIO5 (29) (30) GND
 a0       GPIO6 (31) (32) GPIO12     data2
 data3   GPIO13 (33) (34) GND
         GPIO19 (35) (36) GPIO16     data6
-        GPIO26 (37) (38) GPIO20
-           GND (39) (40) GPIO21
+        GPIO26 (37) (38) GPIO20     choose0
+           GND (39) (40) GPIO21     choose1
 
 
 """
@@ -64,6 +64,7 @@ def init_GPIO():
     global t1
     global t2
     global control
+    global choose
     t0 = "t0"
     t1 = "t1"
     t2 = "t2"
@@ -77,6 +78,7 @@ def init_GPIO():
     # gate1 = LED(8)
     # gate2 = LED(9)
     data = [LED(10),LED(11),LED(12),LED(13),LED(14),LED(15),LED(16),LED(17)]
+    choose = [LED(20),LED(21),LED(22),LED(23),LED(24),LED(25)]
     pass
 
 def exit_GPIO():
@@ -93,7 +95,7 @@ def exit_GPIO():
 # xxx.on() and xxx.off() is efficient enough.
 
 # adress io level
-def output_ram(target="",data_for_output="00000000"):
+def output_ram(target,data_for_output):
     # note that off sometimes means ON
 
     # address
@@ -124,7 +126,7 @@ def output_ram(target="",data_for_output="00000000"):
 
     # WR
     wr.off() # pull to low
-    time.sleep(0.000000100) # >=12ns 0.000000012
+    time.sleep(0.000000100) # >=12ns 0.000000012, set 100 ns here
     wr.on() # rising-edge triggle
 
     # exit
@@ -133,8 +135,6 @@ def output_ram(target="",data_for_output="00000000"):
     rd.on() # double high, high-Z
 
     time.sleep(0.000000100) # may be useless
-
-
 
     pass
 def input_ram(target=""):
@@ -152,8 +152,7 @@ def set_num_registor(num = 100):
         assert False,"too big number for registor"
     bin_ = bin(num)[2:]
     bin_num = ""
-
-    for i in range(16):
+    for i in range(16): # change into 16 bits
         if i < len(bin_):
             bin_num = bin_[-i-1] + bin_num
         else:
@@ -163,10 +162,45 @@ def set_num_registor(num = 100):
     output_ram(control,"00110000") # write two binary number mode, function 0
     output_ram(t0,bin_num[8:]) # low 8 bit
     output_ram(t0,bin_num[:8]) # high 8 bit
-def start():
+def start_count():
     gate0.on() # start counting
     # output starts to be "high" when count to 0
     pass
+def end_count():
+    gate0.off() # end counting
+    # output starts to be "high" when count to 0
+    pass
+
+# board operation level
+def channel_select(channel_index):
+    cs.on() # close chip-choosing at first
+    # change into 8-bit binary num
+    bin_ = bin(channel_index)[2:]
+    bin_num = ""
+    for i in range(8):  # change into 8 bits # add zero
+        if i < len(bin_):
+            bin_num = bin_[-i - 1] + bin_num
+        else:
+            bin_num = "0" + bin_num
+    # set channel choosing pin according to bin_num
+    for i in range(8):
+        if bin_num[index] == "0":
+            choose[index].off()
+        elif bin_num[index] == "1":
+            choose[index].on()
+        else:
+            assert False,"Channel choosing error."
+def input_num_to_board(delay_list=[]):
+    assert len(delay_list) == 64,"delay list length error, not 64."
+    # time-to-number-ratio
+    time_to_number_ratio = 1.0
+    # output
+    for channel_index in range(64):
+        channel_select(channel_index)
+        # add some exchange for time-to-number
+        time_num = int(time_to_number_ratio * delay_list[channel_index])
+        set_num_registor(num=time_num)
+
 
 if __name__ == "__main__":
     # init
@@ -174,21 +208,25 @@ if __name__ == "__main__":
 
     clk_tem = LED(8)
     # main
-    interval = 10
-    for epotch in range(1000):
+    interval = 10 # 0.01s one time
+    for epotch in range(10000):
+        # config registor
         set_num_registor(num=interval)
-        start()
-        for index in range(interval):
-            time.sleep(0.01)
+        start_count()
+
+        # vitural clock ...
+        for index in range(interval+5):
+            time.sleep(0.005)
             clk_tem.on()
-            time.sleep(0.01)
+            time.sleep(0.005)
             clk_tem.off()
             if index % 100 == 0:
                 print("100 term end ...")
 
-        print("end .")
-        time.sleep(1/interval)
-
+        # end
+        end_count()
+        # delay
+        time.sleep(interval*0.01)
 
     # end
     exit_GPIO()
